37a8b410801623a8125d9c25ec79ce42edc10bb0
Manish <manish2003sharma@gmail.com>
2025-08-16 00:45:57 +0530

    components

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import './Header.css';
-// import '../../App.css';
-// import '../../index.css';
+import { Link } from "react-router-dom";
 
 const Header = () => {
     return (
@@ -9,10 +8,21 @@ const Header = () => {
             <div className="top_strip py-2">
                 <div className="container">
                     <div className="flex items-center justify-between">
-                        <div className="col1 w-[50]">
-                            <p className='text-[14px]'>
+                        <div className="col1 w-[50%]">
+                            <p className='text-[14px] font-[500]'>
                                 Get up to 50% off new season styles, limited time only.
                             </p>
+                        </div>
+
+                        <div className="col2 flex items-center justify-end">
+                            <ul>
+                                <li className='list-none'>
+                                    <Link className='text-[12px] link font-[500]' to='#'>Help Center</Link>
+                                </li>
+                                {/* <li className='list-none'>
+                                    <Link className='text-[12px] link' to='#'>Order Tracking</Link>
+                                </li> */}
+                            </ul>
                         </div>
                     </div>
                 </div>
